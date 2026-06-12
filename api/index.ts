f41333603f4b43e9741ac85b15dcf91ddf900879
987tom1 <987tom1@gmail.com>
2026-06-12 19:41:34 +1000

    debug: minimal handler to test Build Output API config

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,4 @@
-import { createAppInstance } from '../src/app';
-
-const appPromise = createAppInstance();
-
-export default async function handler(req: any, res: any): Promise<void> {
-  const app = await appPromise;
-  app(req, res);
+export default function handler(req: any, res: any): void {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ ok: true, build: 'esbuild-cjs-boa' }));
 }
